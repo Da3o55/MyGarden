bae02f7e01a62606873603f0e365d963b64add6b
Alex GIT <Da3o55@laposte.net>
2026-08-21 01:40:57 +0200

    fzesfzf

--- mygarden/run.sh
+++ mygarden/run.sh
@@ -10,7 +10,7 @@ if bashio::services.available "mqtt"; then
     export MQTT_PORT=$(bashio::services mqtt "port")
     export MQTT_USER=$(bashio::services mqtt "username")
     export MQTT_PASS=$(bashio::services mqtt "password")
-    bashio::log.info "MQTT détecté: host=${MQTT_HOST} port=${MQTT_PORT}"
+    bashio::log.info "MQTT détecté: host=${MQTT_HOST} port=${MQTT_PORT} user=${MQTT_USER}"
 else
     bashio::log.warning "MQTT non disponible !"
 fi
@@ -20,5 +20,4 @@ cd /app
 
 export PYTHONUNBUFFERED=1
 
-# TEST : Est-ce que Python existe et marche ?
-exec /venv/bin/python3 -c "print('>>> PYTHON OK'); import sys; print(f'>>> Version: {sys.version}')"
+exec /venv/bin/python3 main.py
